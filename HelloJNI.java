99ff09c4f1030705aaf7c13ebd5c8880ef964f3e
Marek Kolodziej <marek.kolodziej@gonitro.con>
2015-03-02 14:01:51 -0800

    update

--- HelloJNI.java
+++ HelloJNI.java
@@ -3,9 +3,10 @@ public class HelloJNI {
    private native void sayHello();
    
    public static void main(final String[] args) {
-      
-      System.out.printf("\njava.library.path=%s\n\n", System.getProperty("java.library.path"));
-      System.load(args[0]);
-      new HelloJNI().sayHello();  
+
+       System.out.printf("\njava.library.path=%s\n\n", System.getProperty("java.library.path"));
+       // System.loadLibrary("HelloJNI");
+   	   System.load(args[0]);
+       new HelloJNI().sayHello();  
    }
 }
