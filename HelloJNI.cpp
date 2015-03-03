51082a5396d21b1ed7c59c080591cd672bc3808f
Marek Kolodziej <marek.kolodziej@gonitro.con>
2015-03-02 20:28:13 -0800

    Added other primitive types

--- HelloJNI.cpp
+++ HelloJNI.cpp
@@ -4,6 +4,8 @@
 #include "HelloJNI.h"
 
 using namespace std;
+
+typedef unsigned char BYTE;
  
 JNIEXPORT jstring JNICALL Java_HelloJNI_getNativeString(JNIEnv *env, jobject thisObj, jstring scalaStr) {
   
@@ -15,4 +17,60 @@ JNIEXPORT jstring JNICALL Java_HelloJNI_getNativeString(JNIEnv *env, jobject thi
   string in;
   getline(cin, in);
   return env->NewStringUTF((char*)in.c_str());
+}
+
+JNIEXPORT jboolean JNICALL Java_HelloJNI_getNativeBoolean(JNIEnv *env, jobject thisObj, jboolean scalaBool) {
+
+  const bool nativeBool = (bool) scalaBool;
+  cout << "In C++ - received Boolean from Scala: " << boolalpha << nativeBool << endl;
+  return nativeBool;
+}
+
+JNIEXPORT jbyte JNICALL Java_HelloJNI_getNativeByte(JNIEnv *env, jobject thisObj, jbyte scalaByte) {
+
+  const BYTE nativeByte = (BYTE) scalaByte;
+  cout << "In C++ - received byte from Scala: " << (int) nativeByte << endl;
+  return nativeByte;
+}
+
+JNIEXPORT jchar JNICALL Java_HelloJNI_getNativeChar(JNIEnv *env, jobject thisObj, jchar scalaChar) {
+
+  const char nativeChar = (char) scalaChar;
+  cout << "In C++ - received char from Scala: " << nativeChar << endl;
+  return nativeChar;
+}
+
+JNIEXPORT jshort JNICALL Java_HelloJNI_getNativeShort(JNIEnv *env, jobject thisObj, jshort scalaShort) {
+
+  const short nativeShort = (short) scalaShort;
+  cout << "In C++ - received short from Scala: " << nativeShort << endl;
+  return nativeShort;
+}
+
+JNIEXPORT jint JNICALL Java_HelloJNI_getNativeInt(JNIEnv *env, jobject thisObj, jint scalaInt) {
+
+  const int nativeInt = (int) scalaInt;
+  cout << "In C++ - received int from Scala: " << nativeInt << endl;
+  return nativeInt;
+}
+
+JNIEXPORT jlong JNICALL Java_HelloJNI_getNativeLong(JNIEnv *env, jobject thisObj, jlong scalaLong) {
+
+  const long long nativeLong = (long) scalaLong;
+  cout << "In C++ - received long from Scala: " << nativeLong << endl;
+  return nativeLong;
+}
+
+JNIEXPORT jfloat JNICALL Java_HelloJNI_getNativeFloat(JNIEnv *env, jobject thisObj, jfloat scalaFloat) {
+
+  const float nativeFloat = (float) scalaFloat;
+  cout << "In C++ - received float from Scala: " << nativeFloat << endl;
+  return nativeFloat;
+}
+
+JNIEXPORT jdouble JNICALL Java_HelloJNI_getNativeDouble(JNIEnv *env, jobject thisObj, jdouble scalaDouble) {
+
+  const double nativeDouble = (double) scalaDouble;
+  cout << "In C++ - received doubla from Scala: " << nativeDouble << endl;
+  return nativeDouble;
 }
--- HelloJNI.scala
+++ HelloJNI.scala
@@ -1,5 +1,22 @@
 class HelloJNI {
 	
+  @native def getNativeBoolean(scalaBool: Boolean): Boolean
+
+  @native def getNativeByte(scalaByte: Byte): Byte
+
+  @native def getNativeChar(scalaChar: Char): Char
+
+  @native def getNativeShort(scalaShort: Short): Short
+
+  @native def getNativeInt(scalaInt: Int): Int
+
+  @native def getNativeLong(scalaLong: Long): Long
+
+  @native def getNativeFloat(scalaFloat: Float): Float
+
+  @native def getNativeDouble(scalaDouble: Double): Double
+
+  // native representation: modified UTF-8 (http://docs.oracle.com/javase/1.5.0/docs/guide/jni/spec/types.html)
   @native def getNativeString(scalaStr: String): String
 }
 
@@ -7,7 +24,44 @@ object HelloJNI extends App {
 
   println(s"\njava.library.path=${System.getProperty("java.library.path")}\n\n")
   System.load(args(0))
+
+  val helloJNI = new HelloJNI()
+
   print("In Scala - enter string to be sent to C++: ")
   val str = readLine()
-  println(s"\nIn Scala: the C++ user entered: ${new HelloJNI().getNativeString(str)}\n\n")
+  println(s"In Scala: the C++ user entered: ${helloJNI.getNativeString(str)}\n")
+ 
+  val bool = true
+  println(s"In Scala: sending Boolean value of $bool to C++")
+  println(s"In Scala: C++ responded with Boolean value of ${helloJNI.getNativeBoolean(bool)}\n")
+
+  val byte = 65.toByte
+  println(s"""In Scala: sending byte value of $byte (letter ${byte.toChar}) to C++""")
+  println(s"In Scala: C++ responded with byte value of ${helloJNI.getNativeByte(byte)}\n")
+
+  val char = 'A'
+  println(s"""In Scala: sending char $char to C++""")
+  println(s"In Scala: C++ responded with char value of ${helloJNI.getNativeChar(char)}\n")
+  
+  val short = 1.toShort
+  println(s"""In Scala: sending short $short to C++""")
+  println(s"In Scala: C++ responded with short value of ${helloJNI.getNativeShort(short)}\n")
+ 
+  val int = 1
+  println(s"""In Scala: sending int $int to C++""")
+  println(s"In Scala: C++ responded with int value of ${helloJNI.getNativeInt(int)}\n")
+
+  val long = 1L
+  println(s"""In Scala: sending long $long to C++""")
+  println(s"In Scala: C++ responded with long value of ${helloJNI.getNativeLong(long)}\n")
+
+  val float = 1.0F
+  println(s"""In Scala: sending float $float to C++""")
+  println(s"In Scala: C++ responded with float value of ${helloJNI.getNativeFloat(float)}\n")
+ 
+  val double = 1.0D
+  println(s"""In Scala: sending double $double to C++""")
+  println(s"In Scala: C++ responded with doublt value of ${helloJNI.getNativeDouble(double)}\n")
 }
+
+
