a254f9dd1c73744c23d9c95b9199fc8490cd75aa
Marek Kolodziej <marek.kolodziej@gonitro.con>
2015-11-14 20:48:32 -0800

    Refactoring

--- HelloStatefulJNI.cpp
+++ HelloStatefulJNI.cpp
@@ -3,21 +3,17 @@
 #include "HelloStatefulJNI.h"
 
 JNIEXPORT jlong JNICALL Java_HelloStatefulJNI_malloc(JNIEnv * const env, jobject const thisObj, const jint numEl) {
-	int * ptr = (int *) malloc(numEl * sizeof(int));
-	return (jlong) ptr;
+	return (jlong) (int *) malloc(numEl * sizeof(int));
 }
 
 JNIEXPORT void JNICALL Java_HelloStatefulJNI_putInt(JNIEnv * const env, jobject const thisObj, const jlong ptrAddr, const jint pos, const jint value) {
-	int * ptr = (int *) ptrAddr;
-	*(ptr + pos) = value;
+	*((int *) ptrAddr + pos) = value;
 }
 
 JNIEXPORT jint JNICALL Java_HelloStatefulJNI_getInt(JNIEnv * const env, jobject const thisObj, const jlong ptrAddr, const jint pos) {
-	int * ptr = (int *) ptrAddr;
-    return (jint) *(ptr + pos);
+    return (jint) *((int *) ptrAddr + pos);
 }
 
 JNIEXPORT void JNICALL Java_HelloStatefulJNI_free(JNIEnv * const env, jobject const thisObj, const jlong ptrAddr) {
-	int * ptr = (int *) ptrAddr;
-	free(ptr);
+	free((int *) ptrAddr);
 }
